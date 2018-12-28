67d51c03783d3a1e1a169704a7175a4d40804b2c
matt <mdailis@ccs.neu.edu>
2018-12-28 00:07:41 -0500

    Add image of matt playing guitar

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 
-render(<div>Hello world!</div>, document.getElementById("root"));
+import profile_pic from '../images/mattguitar.jpg'
+
+render(<div><h1>Matt's personal website</h1><img src={profile_pic} /></div>, document.getElementById("root"));

72a97e359997f4f04e52347522766f74825d21d2
matt <mdailis@ccs.neu.edu>
2018-12-28 01:21:43 -0500

    Add initial resume

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,8 @@ import { render } from 'react-dom';
 
 import profile_pic from '../images/mattguitar.jpg'
 
-render(<div><h1>Matt's personal website</h1><img src={profile_pic} /></div>, document.getElementById("root"));
+render(
+    <div>
+        <h1>Matt's personal website</h1>
+        <img src={profile_pic} />
+    </div>, document.getElementById("root"));
